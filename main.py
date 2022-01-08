65689410af7b2e7cdd527aab0b9a12e89c0c1507
Ilya Yudin <nibezo.cs@gmail.com>
2022-01-08 19:18:23 +0300

    feat: experiments with lists and message dates

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import telebot
-files_ids = []
+
+media_group_id = []
+message_id = []
+date = []
 bot = telebot.TeleBot("5036901574:AAHr80p-rd6BbjXT6h40YsOLybZjxeJ3gM4", parse_mode=None)
+x = 0
+
+
+def all_the_same(elements):
+    # return True if all elements the same
+    return len(elements) < 1 or len(elements) == elements.count(elements[0])
 # 1111 - -763688879
 # main - -783236302
 # C2 - -757448895
@@ -19,10 +28,15 @@ def forward_text(message):
             bot.forward_message('-763688879', '-783236302', message.id)
 
 
+'''
+1. Check message_group_id and member it
+2. Append to files_ids ids of the files
+'''
+
+
 # media_group_id for media group
 @bot.message_handler(content_types=['photo'])
 def forward_text(message):
-    print(message)
     if str(message.chat.id) == '-783236302' and str(message.media_group_id) == 'None':
         if 'с4' in str(message.caption).lower() or 'c4' in str(message.caption).lower():
             bot.forward_message('-747350870', '-783236302', message.id)
@@ -30,21 +44,43 @@ def forward_text(message):
             bot.forward_message('-757448895', '-783236302', message.id)
         elif '1111' in str(message.caption):
             bot.forward_message('-763688879', '-783236302', message.id)
-        print('This is not media group!')
+        print('This is not media group!\n')
 
     # For media group
-    print(message.photo[-1].file_id) # get photo id
+    # print(message.photo[-1].file_id)  # get photo id
     #
-    if str(message.chat.id) == '-783236302' and str(message.media_group_id) != 'None':
-        print()
-        files_ids.append(str(message.photo[-1].file_id))
-        if str(message.photo[-1].file_id) not in files_ids:
-            print('OK')
+    # if str(message.chat.id) == '-783236302' and str(message.media_group_id) != 'None':
+    #     print(message.photo[-1].file_id)
+    #     print(message.photo[-1])
+    #     print(message.photo)
+    #     print(len(message.photo))
+    #     files_ids.append(str(message.photo[-1].file_id))
+    # if str(message.photo[-1].file_id) not in files_ids:
+    #     print('OK')
 
-    print(files_ids)
+    # print(files_ids)
     # if 'с4' in str(message.caption).lower() or 'c4' in str(message.caption).lower():
     #     bot.forward_message('-747350870', '-783236302', message.id)
-    print('This is media group!')
+    if str(message.chat.id) == '-783236302' and str(message.media_group_id) != 'None':
+        # print(message.media_group_id, '\n')
+        # print('This is media group!\n')
+        media_group_id.append(str(message.media_group_id))
+        message_id.append(str(message.id))
+        date.append(str(message.date))
 
-print('s')
+        # Forward the 1st photo to the specified chat
+        if 'с4' in str(message.caption).lower() or 'c4' in str(message.caption).lower():
+            bot.forward_message('-747350870', '-783236302', message.id)
+        elif 'с2' in str(message.caption).lower() or 'c2' in str(message.caption).lower():
+            bot.forward_message('-757448895', '-783236302', message.id)
+        elif '1111' in str(message.caption):
+            bot.forward_message('-763688879', '-783236302', message.id)
+        elif '' in str(message.caption):
+            print('NEXT PHOTO(S)\n')
+            print(media_group_id, '\n')
+            print(date, '\n')
+            print(message_id, '\n')
+            print(all_the_same(date))
+
+
 bot.infinity_polling()
