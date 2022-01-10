3f271f5b213a0ddeb31705c2d1951b94c75e4254
Ilya Yudin <nibezo.cs@gmail.com>
2022-01-10 12:34:38 +0300

    fix: fix chat's ids

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ def check(message):
 def forward_text(message):
     if str(message.chat.id == '-1001697120368') and str(message.text).lower() == 'да':
         bot.reply_to(message, "Провода")
-    if str(message.chat.id) == '-783236302':
+    if str(message.chat.id) == '-1001697120368':
         if 'с4' in str(message.text).lower() or 'c4' in str(message.text).lower():
-            bot.forward_message('-747350870', '-783236302', message.id)
+            bot.forward_message('-1001793220760', '-1001697120368', message.id)
         elif 'с2' in str(message.text).lower() or 'c2' in str(message.text).lower():
-            bot.forward_message('-757448895', '-783236302', message.id)
+            bot.forward_message('-1001572181895', '-1001697120368', message.id)
         elif '1111' in str(message.text):
-            bot.forward_message('-763688879', '-783236302', message.id)
+            bot.forward_message('-1001553453488', '-1001697120368', message.id)
 
 
 # media_group_id for media group
@@ -29,18 +29,18 @@ def forward_text(message):
         elif 'с2' in str(message.caption).lower() or 'c2' in str(message.caption).lower():
             bot.forward_message('-1001572181895', '-1001697120368', message.id)
         elif '1111' in str(message.caption):
-            bot.forward_message('-608096809', '-1001697120368', message.id)
+            bot.forward_message('-1001553453488', '-1001697120368', message.id)
         print('This is not media group!\n')
 
     # For media group
-    if str(message.chat.id) == '-783236302' and str(message.media_group_id) != 'None':
+    if str(message.chat.id) == '-1001697120368' and str(message.media_group_id) != 'None':
         # Forward the 1st photo to the specified chat
         if 'с4' in str(message.caption).lower() or 'c4' in str(message.caption).lower():
             bot.forward_message('-1001793220760', '-1001697120368', message.id)
         elif 'с2' in str(message.caption).lower() or 'c2' in str(message.caption).lower():
             bot.forward_message('-1001572181895', '-1001697120368', message.id)
         elif '1111' in str(message.caption):
-            bot.forward_message('-608096809', '-1001697120368', message.id)
+            bot.forward_message('-1001553453488', '-1001697120368', message.id)
 
 
 bot.infinity_polling()
